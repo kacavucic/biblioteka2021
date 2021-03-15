c15e53351bb8e4414bc3d82ab19fc3f7bda22a0b
kacavucic <vucic.kat@gmail.com>
2021-03-15 15:46:43 +0100

    Dokumentovana klasa Biblioteka

--- src/biblioteka/Biblioteka.java
+++ src/biblioteka/Biblioteka.java
@@ -4,8 +4,17 @@ import java.util.LinkedList;
 
 import biblioteka.interfejs.BibliotekaInterface;
 
+/**
+ * Ova klasa implementira interfejs BibliotekaInterface i predstavlja implementaciju preko liste.
+ * 
+ * @author Lenovo Yoga
+ *
+ */
 public class Biblioteka implements BibliotekaInterface {
 
+	/**
+	 * Lista sa svim knjigama u biblioteci.
+	 */
 	private LinkedList<Knjiga> knjige = new LinkedList<Knjiga>();
 
 	@Override
