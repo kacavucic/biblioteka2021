bcb6e558f55357664e8e67f845c8f13933cd20f1
kacavucic <vucic.kat@gmail.com>
2021-03-15 15:39:43 +0100

    Dokumentovana klasa Autor

--- src/biblioteka/Autor.java
+++ src/biblioteka/Autor.java
@@ -1,26 +1,63 @@
 package biblioteka;
 
 import java.util.Objects;
-
+/**
+ * Klasa koja predstavlja autora knjige.
+ * 
+ * Autor ima ime i prezime kao String vrednosti.
+ * 
+ * @author Lenovo Yoga
+ * @version 0.1
+ *
+ */
 public class Autor {
 
+	/**
+	 * Ime autora kao String.
+	 */
 	private String ime;
+	
+	/**
+	 * Prezime autora kao String.
+	 */
 	private String prezime;
 
+	/**
+	 * Konstruktor koji inicijalizuje objekat i nista vise.
+	 */
 	public Autor() {
 		super();
 	}
 
+	/**
+	 * Konstruktor koji inicijalizuje objekat i postavlja vrednosti za ime i prezime autora.
+	 * 
+	 * @param ime Ime autora kao String
+	 * @param prezime Prezime autora kao String
+	 */
 	public Autor(String ime, String prezime) {
 		super();
 		setIme(ime);
 		setPrezime(prezime);
 	}
 
+	/**
+	 * Vraca ime autora.
+	 * 
+	 * @return Ime autora kao String
+	 */
 	public String getIme() {
 		return ime;
 	}
 
+	/**
+	 * Postavlja ime autora na novu vrednost.
+	 * 
+	 * @param ime Ime autora kao String
+	 * 
+	 * @throws java.lang.NullPointerException ako je uneto ime null
+	 * @throws java.lang.RuntimeException ako je uneto ime koje ima manje od dva znaka
+	 */
 	public void setIme(String ime) {
 		if (ime == null) {
 			throw new NullPointerException("Ime ne sme biti null");
@@ -31,10 +68,23 @@ public class Autor {
 		this.ime = ime;
 	}
 
+	/**
+	 * Vraca prezime autora.
+	 * 
+	 * @return Prezime autora kao String
+	 */
 	public String getPrezime() {
 		return prezime;
 	}
 
+	/**
+	 * Postavlja prezime autora na novu vrednost.
+	 * 
+	 * @param prezime Prezime autora kao String
+	 * 
+	 * @throws java.lang.NullPointerException ako je uneto prezime null
+	 * @throws java.lang.RuntimeException ako je uneto prezime koje ima manje od dva znaka
+	 */
 	public void setPrezime(String prezime) {
 		if (prezime == null) {
 			throw new NullPointerException("Prezime ne sme biti null");
@@ -45,6 +95,9 @@ public class Autor {
 		this.prezime = prezime;
 	}
 
+	/**
+	 * @return Vraca String sa svim podacima o autoru
+	 */
 	@Override
 	public String toString() {
 		return "Autor [ime=" + ime + ", prezime=" + prezime + "]";
@@ -55,6 +108,17 @@ public class Autor {
 		return Objects.hash(ime, prezime);
 	}
 
+	/**
+	 * Poredi dva autora i vraca true ako su isti, a false ako nisu.
+	 * 
+	 * Autori se porede po imenu i prezimenu i oba moraju da budu ista.
+	 * 
+	 * @return 
+	 * <ul>
+	 * 		<li>true ako su oba objekta klase Autor i imaju ista imena i prezimena</li>
+	 * 		<li>false u svim ostalim slucajevima</li>
+	 * </ul>
+	 */
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj) {
